7ba625a76ee3179a7691aa5bcfe8da85b398430e
Vladimir <vladimir@vladimir-Kubuntu-16.04>
2018-05-01 22:31:10 +0700

    add correlation.py

---
+++ multideterminant.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+if sys.version_info > (3, 0):
+    from functools import reduce
+
+
+class SectionNotFound(Exception):
+    """Section not found in MOLDEN file."""
+
+    def __init__(self, section_name):
+        self.section_name = section_name
+
+    def __str__(self):
+        return repr(self.section_name)
+
+
+class ORCASectionNotFound(Exception):
+    """Section not found in ORCA Output file."""
+
+    def __init__(self, section_name):
+        self.section_name = section_name
+
+    def __str__(self):
+        return repr(self.section_name)
+
+
+class Orca:
+    """
+    ORCA 4.X
+    """
+    title = "generated from Orca output data"
+
+    def __init__(self, orca_input_path):
+        """Initialise multi-determinant support."""
+        self.internal = 0  # CASSCF internal orbitals
+        self.active = 0  # CASSCF active orbitals
+        self.spin_determinants = []  # CASSCF spin-determinants
+        self.orca_input_path = orca_input_path
+        self.parse_output()
+        # self.check_monotonic()
+
+    def orca_section(self, section_name):
+        """
+        :returns: content of named section
+        """
+        with open(self.orca_input_path, "r") as orca_input:
+            orca_input.seek(0)
+            line = orca_input.readline()
+            while line and not line.startswith(section_name):
+                line = orca_input.readline()
+            if not line:
+                raise SectionNotFound(section_name)
+            result = [line]
+            line = orca_input.readline()
+            line = orca_input.readline()
+            while line and not line.startswith('-------'):
+                result.append(line)
+                line = orca_input.readline()
+            return result
+
+    def parse_output(self):
+        """Retrive from ORCA output:
+            Spin-Determinant information
+            number of active & internal orbitals in CASSCF
+
+        because of this issue https://orcaforum.cec.mpg.de/viewtopic.php?f=8&t=3212
+        ORCA input should be:
+        ! CASSCF cc-pVQZ
+
+        %casscf
+          nel  3
+          norb 4
+          PrintWF det
+        end
+
+        * xyzfile 0 2 ../../mol.xyz
+
+        $new_job
+        %casscf
+          nel  3
+          norb 4
+          PrintWF csf
+        end
+
+        * xyzfile 0 2 ../../mol.xyz
+
+        Keep your attention on not using symmetry because of this issue
+        https://orcaforum.cec.mpg.de/viewtopic.php?f=11&t=3254
+
+        :return: list of spin-determinants, number of internal orbitals
+        """
+
+        determinants = {}
+
+        def ternary2decimal(ternary):
+            """Convert ternary string to decimal.
+            '001122' = 1 * 2 + 3 * 2 + 9 * 1 + 27 * 1 = 44
+            '21102' -> 200
+            """
+            return reduce(lambda x, y: x * 3 + int(y), ternary, 0)
+
+        with open(self.orca_input_path, "r") as orca_input:
+            line = orca_input.readline()
+            key = None
+            while line and not line.startswith('  Spin-Determinant CI Printing'):
+                line = orca_input.readline()
+                if line.startswith('   Internal'):
+                    self.internal = int(line.split()[5])
+                if line.startswith('   Active'):
+                    self.active = int(line.split()[5])
+            if not line:
+                raise ORCASectionNotFound('Spin-Determinant CI Printing')
+            line = orca_input.readline()
+            while line and not line.startswith('DENSITY MATRIX'):
+                if line.startswith('CFG['):
+                    key = line.split()[0][4:-1]
+                    determinants[key] = []
+                elif line.startswith('   ['):
+                    det, val = line.split()
+                    determinants[key].append({det[1:-1]: float(val)})
+                line = orca_input.readline()
+
+        with open(self.orca_input_path, "r") as orca_input:
+            line = orca_input.readline()
+            key = None
+            while line and not line.startswith('  Extended CI Printing'):
+                line = orca_input.readline()
+            if not line:
+                raise ORCASectionNotFound('Extended CI Printing')
+            line = orca_input.readline()
+            while line and not line.startswith('DENSITY MATRIX'):
+                if line.startswith(' CFG['):
+                    if determinants.get(line.split()[1]) == []:
+                        key = line.split()[1]
+                elif key and line.startswith(' \tCSF['):
+                    determinants[key] = float(line.split()[1])
+                    key = None
+                line = orca_input.readline()
+
+        self.spin_determinants = []
+        # Sort spin-determinants by increasing occupation levels
+        for k, v in sorted(determinants.items(), key=lambda x: ternary2decimal(x[0][::-1])):
+            if isinstance(v, list):
+                for v1 in v:
+                    self.spin_determinants += [list(v1.items())[0]]
+            else:
+                self.spin_determinants += [(k, v)]
+
+    def check_monotonic(self):
+        """Check if energy of orbitals increases monotonically."""
+        section_body = self.orca_section("ORBITAL ENERGIES")[3:]
+        energy = None
+        for line in section_body:
+            split_line = line.split()
+            if line.isspace():
+                break
+            elif len(split_line) == 4:
+                if energy and float(split_line[2]) < energy:
+                    print(line, energy)
+                energy = float(split_line[2])
+
+    @staticmethod
+    def get_promotion_rules(spin_det_1, spin_det_2):
+        """Creates promotions rules for two spin-determinants
+
+        for '2u00' ->, '0u20'
+        promotion rules is
+            [(1, 1, 3), (2, 1, 3)]
+        for rule (1, 1, 3)
+            first is spin (1=up/2=down)
+            second is number of orbital in active space electron promote from
+            third is of orbital in active space electron promote to
+        orbital numeration starting from 1.
+        """
+        # divide spin-determinants into spin parts
+        spin_det_1_up = [e in ('2', 'u') for e in spin_det_1]
+        spin_det_1_down = [e in ('2', 'd') for e in spin_det_1]
+
+        spin_det_2_up = [e in ('2', 'u') for e in spin_det_2]
+        spin_det_2_down = [e in ('2', 'd') for e in spin_det_2]
+
+        # get difference between initial and final spin-det
+        spin_det_up = [x - y for x, y in zip(spin_det_1_up, spin_det_2_up)]
+        spin_det_down = [x - y for x, y in zip(spin_det_1_down, spin_det_2_down)]
+
+        # get indexes of releasing and filling orbitals
+        spin_det_1_up_indexes = [i+1 for i, x in enumerate(spin_det_up) if x == 1]
+        spin_det_2_up_indexes = [i+1 for i, x in enumerate(spin_det_up) if x == -1]
+
+        spin_det_1_down_indexes = [i+1 for i, x in enumerate(spin_det_down) if x == 1]
+        spin_det_2_down_indexes = [i+1 for i, x in enumerate(spin_det_down) if x == -1]
+
+        return (
+            [(1, fr, to) for fr, to in zip(spin_det_1_up_indexes, spin_det_2_up_indexes)] +
+            [(2, fr, to) for fr, to in zip(spin_det_1_down_indexes, spin_det_2_down_indexes)]
+        )
+
+    @property
+    def ground_state(self):
+        """Lowest occupation spin-determinant"""
+        return self.spin_determinants[0][0]
+
+    def correlation(self):
+        """
+        :returns: MDET section of correlation.data file
+        """
+        with open('correlation.data', 'w') as output_file:
+
+            print('START MDET', file=output_file)
+            print('Title', file=output_file)
+            print(' multideterminant WFN %s\n' % self.title, file=output_file)
+
+            print('MD', file=output_file)
+            print('  %i' % len(self.spin_determinants), file=output_file)
+
+            for i, (_, weight) in enumerate(self.spin_determinants):
+                print(' %9.6f  %i %i' % (weight, i+1, int(i > 0)), file=output_file)
+
+            for i, (spin_det, _) in enumerate(self.spin_determinants):
+                if self.ground_state != spin_det:
+                    for s, f, t in self.get_promotion_rules(self.ground_state, spin_det):
+                        print('  DET %i %i PR %i 1 %i 1' % (i+1, s, f + self.internal, t + self.internal), file=output_file)
+            print('END MDET', file=output_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="This script converts multideterminant information to a CASINO correlation.data file.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        'code', type=int, help=(
+            "number corresponding to the quantum chemistry code used to produce this MOLDEN file:\n"
+            "3 -- ORCA 3.X - 4.X\n"
+        )
+    )
+
+    parser.add_argument('input_file', type=str, help="path to ORCA output file")
+    args = parser.parse_args()
+
+    if args.code == 3:
+        Orca(args.input_file).correlation()
+
+
+if __name__ == "__main__":
+    main()
